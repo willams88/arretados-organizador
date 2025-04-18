1dfd5a58056ebaf3a83cf68094e553085ac5a3ad
willams88 <willamsdiassilva88@gmail.com>
2025-04-18 18:28:51 -0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 
+// Substitua pelos seus dados reais do Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCgFN0lCf0xeJaLnWkTfjAbvDBwQXxXe0A",
-  authDomain: "arretados-ingressos-app.firebaseapp.com",
-  projectId: "arretados-ingressos-app",
-  storageBucket: "arretados-ingressos-app.appspot.com",
-  messagingSenderId: "309467408671",
-  appId: "1:309467408671:web:d4be567df9b4d39a1e282a",
-  measurementId: "G-R5V0QVJBB5"
+  apiKey: "sua-apiKey",
+  authDomain: "seu-authDomain",
+  projectId: "seu-projectId",
+  storageBucket: "seu-storageBucket",
+  messagingSenderId: "seu-messagingSenderId",
+  appId: "seu-appId"
 };
-
-export const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
